ba3de13e23db768e5a903b245eb30183eb9a870b
EC2 Default User <ec2-user@ip-172-31-25-189.ap-northeast-2.compute.internal>
2020-07-14 11:41:56 +0000

    commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String test(){
+        return "hello this is minholee";
+    }
+}
