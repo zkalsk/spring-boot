d0a1571b7db1590935bca656f765bcc614796ee7
EC2 Default User <ec2-user@ip-172-31-25-189.ap-northeast-2.compute.internal>
2020-07-15 07:29:14 +0000

    DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootTest
+@SpringBootApplication
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
